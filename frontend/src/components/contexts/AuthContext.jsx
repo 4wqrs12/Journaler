153ed54016fd5b7c4caeb1090cc0f31632a3480b
4wqrs12 <forzstreet61@gmail.com>
2025-08-31 14:31:17 -0400

    authcontext

---
+++ frontend/src/components/contexts/AuthContext.jsx
@@ -0,0 +1,68 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [token, setToken] = useState(null);
+
+  async function login(username, password) {
+    try {
+      const res = await fetch(`${import.meta.env.VITE_BASE_API}/api/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ username, password }),
+      });
+      if (!res.ok) {
+        throw new Error(`Server error: ${res.status}`);
+      }
+      const json = await res.json();
+      setToken(json.accessToken);
+    } catch (e) {
+      console.error(`Error: ${e}`);
+    }
+  }
+
+  async function logout() {
+    try {
+      await fetch(`${import.meta.env.VITE_BASE_API}/api/logout`, {
+        method: "POST",
+        credentials: "include",
+      });
+
+      setToken(null);
+    } catch (e) {
+      console.error(`Error: ${e}`);
+    }
+  }
+
+  async function refreshToken() {
+    try {
+      const res = await fetch(`${import.meta.env}/api/refresh`, {
+        method: "POST",
+        credentials: "include",
+      });
+
+      if (res.ok) {
+        const json = await res.json();
+        setToken(json.accessToken);
+      } else {
+        setToken(null);
+      }
+    } catch (e) {
+      console.error(`Error: ${e}`);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout, refreshToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
