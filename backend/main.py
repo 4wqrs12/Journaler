863fce6536bebdb61af55db11886fd24a9a69564
4wqrs12 <forzstreet61@gmail.com>
2026-02-18 15:49:46 -0500

    made journals be added to mongodb

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -12,7 +12,19 @@ journals = db["journals"]
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/api/new-journal", methods=["GET","POST"])
+def new_journal():
+    data = request.get_json()
+    journal_name = data.get("journalName", "")
 
+    if not journal_name:
+        return jsonify({"success": False, "message": "No name given", "data": data})
+    
+    if journals.find_one({"name": journal_name}):
+        return jsonify({"success": False, "message": "This journal already exists", "data": data})
+    
+    journals.insert_one({"name": journal_name, "text": ""})
+    return jsonify({"success": True, "message": "Journal created", "data": data})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/components/NewJournalPage.jsx
+++ frontend/src/components/NewJournalPage.jsx
@@ -7,21 +7,48 @@ function NewJournal() {
     setJournalName(e.target.value);
   }
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:5000/api/new-journal", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ journalName }),
+      });
+      const data = await res.json();
+      if (data.success === true) {
+        setJournalName("");
+        alert(data.message);
+      } else {
+        alert(data.message);
+      }
+    } catch (e) {
+      console.log(`An error has occurred: ${e}`);
+    }
+  }
+
   return (
     <div className="route-content">
       <h1 className="route-title">New Journal</h1>
-      <input
-        type="text"
-        value={journalName}
-        onChange={handleNameChange}
-        placeholder="Journal name..."
-        className="focus:outline-none mt-5"
-      />
-      {journalName && (
-        <button className="ml-5 rounded-2xl bg-[#E1C16E] px-5 cursor-pointer hover:bg-[#c0a35b]">
-          Submit
-        </button>
-      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={journalName}
+          onChange={handleNameChange}
+          placeholder="Journal name..."
+          className="focus:outline-none mt-5"
+        />
+        {journalName && (
+          <button
+            type="submit"
+            className="ml-5 rounded-2xl bg-[#E1C16E] px-5 cursor-pointer hover:bg-[#c0a35b]"
+          >
+            Submit
+          </button>
+        )}
+      </form>
     </div>
   );
 }
